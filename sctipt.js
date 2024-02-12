2d6328b7dccbab9375f5ebe353d396db5e883456
Mariam <mariam.mantidze1@gmail.com>
2024-02-12 23:44:32 +0400

    add: countdown logic to JS

--- sctipt.js
+++ sctipt.js
@@ -1,2 +1,29 @@
-let days = document.getElementById("days");
-days.innerText = "2";
+// let days = document.getElementById("days");
+// let hours = document.getElementById("hours");
+// let minutes = document.getElementById("minutes");
+// let seconds = document.getElementById("seconds");
+
+// console.log(now, targetDate, difference);
+
+const now = new Date();
+const targetDate = new Date(now.getTime() + 14 * 24 * 60 * 60 * 1000);
+
+let countDown = setInterval(function () {
+  const now = new Date();
+
+  const difference = targetDate - now;
+
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+  console.log(days, hours, minutes, seconds);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+}, 1000);
